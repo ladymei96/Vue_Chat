7104cda070cfd9480021097b52ae27da2ec2f1e6
MeiChang <uj99850@gmail.com>
2022-05-24 14:22:15 +0800

    init

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { fileURLToPath, URL } from "url";
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "/Vue_Chat",
+  server: {
+    port: 8080,
+    open: true,
+  },
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
