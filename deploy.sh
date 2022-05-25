026ae88cb7093f620ce1f58aab51469805cff54d
MeiChang <uj99850@gmail.com>
2022-05-25 16:29:52 +0800

    add deplot script

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+
+# 发生错误时终止
+set -e
+
+# 构建
+npm run build
+
+# 进入构建文件夹
+cd dist
+
+# 如果你要部署到自定义域名
+# echo 'www.example.com' > CNAME
+
+git init
+git checkout -b main
+# git checkout main
+git add -A
+git commit -m 'build(dist): update bundle'
+
+# 如果你要部署在 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+# 如果你要部署在 https://<USERNAME>.github.io/<REPO>
+git push -f https://github.com/ladymei96/Vue_Chat.git main:gh-pages
+
+cd -
